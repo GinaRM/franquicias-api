764048058eb562c0270a1fce3720dcac175fd2cc
GinaRM <ginarm12@hotmail.com>
2025-07-03 16:27:15 -0500

    chore(deps): switch from MongoDB to R2DBC MySQL

--- src/main/java/com/gina/franquicias_api/infrastructure/adapter/out/mogodb/mapper/FranchiseMapper.java
+++ src/main/java/com/gina/franquicias_api/infrastructure/adapter/out/mogodb/mapper/FranchiseMapper.java
@@ -4,8 +4,23 @@ import com.gina.franquicias_api.domain.model.Franchise;
 import com.gina.franquicias_api.infrastructure.adapter.out.mogodb.entity.FranchiseDocument;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+
 @Component
 public class FranchiseMapper {
-    public FranchiseDocument toDocument(Franchise f) { /* mapea campos */ return null;}
-    public Franchise toDomain(FranchiseDocument d) { /* mapea recursivamente Branch/Products */ return null; }
+    public FranchiseDocument toDocument(Franchise f) {
+        FranchiseDocument d = new FranchiseDocument();
+        d.setId(f.getId());
+        d.setName(f.getName());
+
+        d.setBranches(Collections.emptyList());
+        return d;
+    }
+    public Franchise toDomain(FranchiseDocument d) {
+        return new Franchise(
+                d.getId(),
+                d.getName(),
+                Collections.emptyList()
+        );
+    }
 }
