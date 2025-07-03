b7e9720af5ffa625cc27f09b0383a9c87bb9f183
GinaRM <ginarm12@hotmail.com>
2025-07-03 18:36:57 -0500

    feat(domain): add ProductWithBranch DTO and findMaxStock signature

--- src/main/java/com/gina/franquicias_api/domain/model/ProductWithBranch.java
+++ src/main/java/com/gina/franquicias_api/domain/model/ProductWithBranch.java
@@ -1,2 +1,30 @@
-package com.gina.franquicias_api.domain.model;public class ProductWithBranch {
+package com.gina.franquicias_api.domain.model;
+
+public class ProductWithBranch {
+    private String branchName;
+    private Product product;
+
+    public ProductWithBranch() {
+    }
+
+    public ProductWithBranch(String branchName, Product product) {
+        this.branchName = branchName;
+        this.product = product;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
--- src/main/java/com/gina/franquicias_api/domain/port/in/FranchiseService.java
+++ src/main/java/com/gina/franquicias_api/domain/port/in/FranchiseService.java
@@ -1,10 +1,19 @@
 package com.gina.franquicias_api.domain.port.in;
 
+import com.gina.franquicias_api.domain.model.Branch;
 import com.gina.franquicias_api.domain.model.Franchise;
+import com.gina.franquicias_api.domain.model.Product;
+import com.gina.franquicias_api.domain.model.ProductWithBranch;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface FranchiseService {
     Mono<Franchise> createFranchise(String name);
     Mono<Franchise> addBranch(String franchiseId, String branchName);
+    Mono<Branch>  addProduct(String franchiseId, String branchId, String productName, int stock);
+    Mono<Branch>  removeProduct(String franchiseId, String branchId, String productId);
+    Mono<Product> updateStock(String franchiseId, String branchId, String productId, int newStock);
+    Flux<ProductWithBranch> findMaxStock(String franchiseId);
+
 
 }
